a924b181b09713edad140062b51046d0b2c2482b
Joseph Kao <josephtkao@gmail.com>
2020-03-31 22:08:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,17 @@
 # code your #position_taken? method here!
+<<<<<<< HEAD
 def position_taken?(board,index)
   if (board[index] == "" || board[index] == " " || board[index] == nil)
     puts "false"
     return false
   else
     puts "true"
+=======
+def position_taken(board, index)
+  if board[index] == "" || board[index] == " "
+    return false
+  else
+>>>>>>> 3d1c251709ac0beaf4394310295c49afa8b273db
     return true
   end
 end
